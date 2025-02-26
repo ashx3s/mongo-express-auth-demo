10589300c914a58569060448e78687c3833ec1a0
ashx3s <ashlyn@lilyx.io>
2025-02-25 21:54:09 -0700

    user model added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const connectToDatabase = require("./middleware/connectToDatabase");
 const PORT = process.env.PORT | 3000;
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
